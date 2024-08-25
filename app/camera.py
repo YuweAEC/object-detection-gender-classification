e2b7d1cf86230c1dbfe00d0d5ce244dad31c6237
Yuvraj Singh <yuwe18b@gmail.com>
2024-08-25 20:53:45 +0530

    update bug

--- app/camera.py
+++ app/camera.py
@@ -1,17 +1,16 @@
 import cv2
-import numpy as np
-import tensorflow as tf
 
-class Camera:
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        # Load YOLO and Gender Model (similar to the main.py)
+class VideoCamera:
+    def __init__(self, source=0):
+        # Default source is webcam (0), change to IP address if using mobile camera
+        self.video = cv2.VideoCapture(source)
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        ret, frame = self.video.read()
-        # Add YOLO and Gender Classification Logic (similar to main.py)
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+        success, image = self.video.read()
+        if success:
+            ret, jpeg = cv2.imencode('.jpg', image)
+            return jpeg.tobytes()
+        return None
